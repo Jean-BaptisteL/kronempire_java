c0272ac738fbddc250e810ec138cf742d99c9395
Jean-BaptisteL <jean-baptiste.lagand@laposte.net>
2023-05-08 19:48:11 +0200

    Connection form

--- kronempire/App.js
+++ kronempire/App.js
@@ -1,22 +1,33 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Cat from './Test';
+import Connection from './screens/Connection';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Ceci est un test</Text>
-      <StatusBar style="auto" />
-      <Cat></Cat>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Ceci est un test</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Connection' component={Connection} options={
+          {title: 'KronEmpire - Connexion',
+          headerTitleAlign: 'center'}
+          }/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ kronempire/screens/Connection.js
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { useForm, Controller } from "react-hook-form";
+
+const Connection = () => {
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            email: '',
+            password: ''
+        }
+    });
+    const onSubmit = data => console.log(data);
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text>Connexion</Text>
+                <View>
+                    <Controller
+                        control={control}
+                        rules={{
+                            required: true,
+                            pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i
+                        }}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Adresse mail"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                        name="email"
+                    />
+                    {errors.email && errors.email.type == 'required' && <Text>L'email est obligatoire.</Text>}
+                    {errors.email && errors.email.type == 'pattern' && <Text>L'email doit être valide.</Text>}
+
+                    <Controller
+                        control={control}
+                        rules={{
+                            required: true,
+                        }}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Mot de passe"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                secureTextEntry={true}
+                                multiline={false}
+                                autoCapitalize='none'
+                            />
+                        )}
+                        name="password"
+                    />
+                    {errors.password && errors.password.type == 'required' && <Text>Le mot de passe est obligatoire.</Text>}
+                    <Button title="Se connecter" onPress={handleSubmit(onSubmit)} />
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        // verticalAlign: 'middle'
+    }
+});
+
+export default Connection;
