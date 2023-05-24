29cc7c75fb759d65f3488b8542ea150ed9075fc0
Beaura <nbeaura@gmail.com>
2023-05-24 23:50:42 +0200

    Building tab draft with some style

--- kronempire/styles/Styles.js
+++ kronempire/styles/Styles.js
@@ -5,10 +5,22 @@ const Styles = StyleSheet.create({
     backgroung: {
       flex: 1,
       // justifyContent: "center",
+      // justifyContent: "space-between",
       opacity: 0.5,
     },
     container: {
       flex: 1,
+    },
+    horizontalScrollMenu: {
+      flex: 0.05,
+    },
+    imageValue: {
+      flexDirection: "row",
+    },
+    images: {
+      height: 23,
+      width: 23,
+      marginRight: 3,
     },
     resourcesGlobal: {
       // display: 'flex',
@@ -27,18 +39,12 @@ const Styles = StyleSheet.create({
       justifyContent: "space-evenly",
       // alignItems: "center",
     },
-    imageValue: {
-      flexDirection: "row",
-    },
-    images: {
-      height: 23,
-      width: 23,
-      marginRight: 3,
-    },
     screen: {
         justifyContent: "center",
     },
     screenText: {
+      flex: 0.2,
+
       color: "black",
       fontSize: 25,
       fontFamily: "serif",
@@ -50,6 +56,56 @@ const Styles = StyleSheet.create({
     screenTextView: {
       marginTop: 8,
     },
+
+    // building container
+    // = screenText above
+    tabName: {
+      flex: 0.08,
+      color: "black",
+      fontSize: 25,
+      fontFamily: "serif",
+      fontWeight: "bold",
+      textAlign: "center",
+      opacity: 1,
+      marginTop: 8
+    },
+    buildingLevel: {
+      flex: 0.1,
+
+    },
+    buildingPriceGlobal: {
+      flex: 0.5,
+      flexDirection: 'row'
+
+    },
+    buildingImage: {
+      flex: 0.5,
+      resizeMode: "contain",
+      // padding: 8,
+      marginTop: -250,
+      marginLeft: 10,
+    },
+    buildingPriceIn: {
+      flex: 0.5,
+      justifyContent: "space-evenly",
+      padding: 8,
+    },
+    buildingPrice: {
+      flex: 0.3,
+
+    },
+    buildingLocks: {
+      flex: 0.3,
+
+    },
+    buildingDescription: {
+      flex: 0.4,
+
+    },
+    buildingDescriptionParaph: {
+
+    },
+    // + horizontalScrollMenu see above
   });
 
   export default Styles;
