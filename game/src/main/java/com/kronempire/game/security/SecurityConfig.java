011a5dc4c0c1c7e4e9fa023088105d5934c8d2ab
Jean-BaptisteL <jean-baptiste.lagand@laposte.net>
2023-06-01 17:34:23 +0200

    files connection

--- game/src/main/java/com/kronempire/game/security/SecurityConfig.java
+++ game/src/main/java/com/kronempire/game/security/SecurityConfig.java
@@ -1,13 +1,24 @@
 package com.kronempire.game.security;
 
+import com.kronempire.game.security.service.PlayerSpringService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,20 +26,71 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private PlayerSpringService playerSpringService;
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // TODO configure authentication manager
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    /*
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }*/
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        System.out.println("coucou");
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/player/signin")
+                .permitAll()
+                .requestMatchers("/player/login")
+                .permitAll()
+                .anyRequest().authenticated();
+        /*http.authorizeHttpRequests(
+                auth -> auth
+                        .requestMatchers("/player/signin")
+                        .permitAll()
+                        .anyRequest().authenticated());
+        http.httpBasic(withDefaults());*/
 
-        http.authorizeHttpRequests(
-                auth -> auth.requestMatchers("/player/signin")
-                        .permitAll().anyRequest().authenticated());
-        http.httpBasic(withDefaults());
+        /*http.cors().and().csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/player/signin")
+                .permitAll()
+                .requestMatchers("/player/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);*/
 
-        http.csrf().disable();
+        /*http.authorizeRequests()
+                .requestMatchers("/player/signin")
+                .permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();*/
 
         return http.build();
 
